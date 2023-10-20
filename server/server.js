880bae44c70c10002a9c2553c7a4c4fc559ec28d
ahmadabukatish <130893911+ahmadabukatish@users.noreply.github.com>
2023-10-20 14:32:44 +0300

    make it better

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req,res){
     res.sendFile(
-        path.join(__dirname,"../react-app/build/index.html"),
+        path.join(__dirname,"../react-app/dist/index.html"),
         function(err){
             res.status(500).send(err)
         }
